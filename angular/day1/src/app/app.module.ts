dd517c715ab6559b95578bc9ebb6bca78b407a43
root <nv.ngoc@hutech.edu.vn>
2023-06-27 02:36:58 +0000

    add merterial icon

--- angular/day1/src/app/app.module.ts
+++ angular/day1/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './app-top-bar/app-top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/day1/src/app/product-list/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Products } from '../product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = [...Products]
+}
